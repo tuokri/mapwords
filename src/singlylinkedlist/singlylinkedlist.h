ab3ce6bf04093faaf6c3b6266b32c7ea8a8924a7
Tuomo Kriikkula <tuokri@tuta.io>
2019-02-11 17:16:16 +0200

    Singly linked list

---
+++ src/singlylinkedlist/singlylinkedlist.h
@@ -0,0 +1,10 @@
+#ifndef MAPWORDS_SINGLYLINKEDLIST_H
+#define MAPWORDS_SINGLYLINKEDLIST_H
+
+typedef struct sll_node
+{
+    char* data;
+    struct node* next;
+} sll_node_t;
+
+#endif //MAPWORDS_SINGLYLINKEDLIST_H
