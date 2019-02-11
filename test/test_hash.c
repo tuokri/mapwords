ba788157a8cd1c357d01dcf1e6451ba0452c20b8
Tuomo Kriikkula <tuokri@tuta.io>
2019-02-11 21:54:04 +0200

    Test default CRC32 matches SSE4.2 optimized version

--- test/test_hash.c
+++ test/test_hash.c
@@ -12,16 +12,45 @@ START_TEST(test_hash_djb2)
 }
 END_TEST
 
+START_TEST(test_hash_sdbm)
+{
+    hash_t h1 = hash_sdbm("test1", 6);
+    hash_t h2 = hash_sdbm("test2", 6);
+    fail_if(h1 == h2);
+}
+END_TEST
+
+START_TEST(test_hash_crc32)
+{
+    hash_t h1 = hash_sse42_crc32("test1", 6);
+    hash_t h2 = hash_sse42_crc32("test2", 6);
+    fail_if(h1 == h2);
+
+    hash_t h3 = hash_default_crc32("test1", 6);
+    hash_t h4 = hash_default_crc32("test2", 6);
+    fail_if(h3 == h4);
+
+    fail_if(h1 != h3);
+    fail_if(h2 != h4);
+}
+END_TEST
+
 Suite* hash_suite()
 {
     Suite* s;
-    TCase* tc_core;
+    TCase* tc_basic;
 
     s = suite_create("hash");
-    tc_core = tcase_create("core");
+    tc_basic = tcase_create("hash basic");
 
-    tcase_add_test(tc_core, test_hash_djb2);
-    suite_add_tcase(s, tc_core);
+    tcase_add_test(tc_basic, test_hash_djb2);
+    suite_add_tcase(s, tc_basic);
+
+    tcase_add_test(tc_basic, test_hash_sdbm);
+    suite_add_tcase(s, tc_basic);
+
+    tcase_add_test(tc_basic, test_hash_crc32);
+    suite_add_tcase(s, tc_basic);
 
     return s;
 }
