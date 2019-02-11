233705ba0154b779ed6f650a44e5c82eb7189206
Tuomo Kriikkula <tuokri@tuta.io>
2019-02-11 18:38:36 +0200

    Extend hashmap details

--- src/hashmap/hashmap.h
+++ src/hashmap/hashmap.h
@@ -3,9 +3,25 @@
 
 #include "hash.h"
 
-struct hashmap
+// TODO:
+// Choose initial size for map.
+// Need a prime table for resizing?
+// Store arbitrary data in hashmap with void*?
+
+typedef struct hashmap
 {
     hash_t key;
-};
+    uint32_t value;
+    hash_t (*hash1)(char* buffer, size_t size);
+    hash_t (*hash2)(char* buffer, size_t size);
+} hashmap_t;
+
+hashmap_t*
+init_hashmap
+(
+    size_t init_size,
+    hash_t (*hash1)(char* buffer, size_t size),
+    hash_t (*hash2)(char* buffer, size_t size)
+);
 
 #endif //MAPWORDS_HASHMAP_H
