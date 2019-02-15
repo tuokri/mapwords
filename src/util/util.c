efef6d22ab47536fe4bfa781b1afb32cdaeb5e1b
Tuomo Kriikula <tuokri@tuta.io>
2019-02-15 20:14:59 +0200

    Add util

---
+++ src/util/util.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+#include "util.h"
+
+// Used in constructing a string literal with the preprocessor.
+#define STR_(X)
+#define STR(X) STR_(X)
+
+int
+read_next_word(FILE* f, char* buf, int len)
+{
+    // Consume all non-allowed characters.
+    fscanf(f, "%*[^a-zA-Z']");
+    return fscanf(f, "%" STR(len) "[a-zA-Z']", buf);
+}
+
+void
+str_tolower(char* str)
+{
+    while(*str)
+    {
+        *str = tolower((unsigned char)*str);
+        str++;
+    }
+}
---
+++ src/util/util.h
@@ -0,0 +1,11 @@
+#ifndef MAPWORDS_UTIL_H
+#define MAPWORDS_UTIL_H
+
+int
+read_next_word(FILE* f, char* buf, int len);
+
+void
+str_tolower(char* str);
+
+
+#endif //MAPWORDS_UTIL_H
